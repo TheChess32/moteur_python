48797d3ffc0c29b857b2236e5efec78e89ba6873
TheChess32 <50355515+TheChess32@users.noreply.github.com>
2019-12-19 21:10:02 +0100

    Add files via upload

--- Projet.py
+++ Projet.py
@@ -2,6 +2,8 @@ import tkinter as tk
 import csv
 from random import *
 
+accueil = tk.Tk()
+
 def depuis_csv(fichier):
     fichier1=open(fichier + '.csv','r',encoding='utf-8')
     lecteur = csv.DictReader(fichier1,delimiter=',')
@@ -57,5 +59,23 @@ def createAccount(pseudo, mdp):
         new_donnees["Chiffre de cryptage"] = chiffre_de_cryptage
         accounts.append(new_donnees)
 
-createAccount("test", "Bonsoir")
+def accueil():
+    accueil = tk.Tk()
+    accueil.title("Accueil")
+    accueil.geometry("1080x720")
+    accueil.minsize(540, 360)
+    accueil.maxsize(1080, 720)
+    accueil.iconbitmap("logo.ico")
+    accueil.config(background = "#0EABB8")
+
+    menu_bar = tk.Menu(accueil)
+    file_menu = tk.Menu(menu_bar, tearoff=0)
+    file_menu.add_command(label = "Fermer", command=accueil.quit)
+    menu_bar.add_cascade(label="Menu", menu=file_menu)
+    
+    accueil.config(menu=menu_bar)
+    
+    accueil.mainloop()
+
+accueil()
 vers_csv("accounts", ["Pseudo", "Mot de passe", "Chiffre de cryptage"])
